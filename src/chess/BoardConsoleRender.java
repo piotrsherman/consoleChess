78225ed6fe0b2a38f69c6edcfd2a0d48db2af254
piotrsherman <piotr.s.herman@gmail.com>
2023-02-25 22:39:27 +0100

    new render logic + piece highlight

--- src/chess/BoardConsoleRender.java
+++ src/chess/BoardConsoleRender.java
@@ -2,6 +2,8 @@ package chess;
 
 import chess.piece.Piece;
 
+import java.util.Set;
+
 public class BoardConsoleRender {
 
     public static final String ANSI_RESET = "\u001B";
@@ -9,15 +11,18 @@ public class BoardConsoleRender {
     public static final String ANSI_BLACK_PIECE_COLOR = "\u001B[30m";
     public static final String ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m";
     public static final String ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m";
-     public void render(Board board){
+    public static final String ANSI_HIGHLIGHTED_SQUARE_BACKGROUND = "u\001B[47m";
+     public void render(Board board, Piece pieceToMove){
+         Set<Coordinates> availableMoveSquares = pieceToMove.getAvailibleMoveSquares(board);
          for(int rank = 8; rank >= 1; rank--){
              String line = "";
              for(File file : File.values()){
                 Coordinates coordinates = new Coordinates (file, rank);
+                boolean isHighlight = availableMoveSquares.contains(coordinates);
                 if (board.isSquareEmpty(coordinates)){
-                    line += getSpriteForEmptySquare(new Coordinates(file, rank));
+                    line += getSpriteForEmptySquare(coordinates, isHighlight);
                 } else{
-                    line += getPieceSprite(board.getPiece(coordinates));
+                    line += getPieceSprite(board.getPiece(coordinates), isHighlight);
                 }
              }
 
@@ -26,7 +31,7 @@ public class BoardConsoleRender {
          }
      }
 
-     private String colorizeSprite(String sprite, Color pieceColor, boolean isSquareDark){
+     private String colorizeSprite(String sprite, Color pieceColor, boolean isSquareDark, boolean isHighlighted){
         String result = sprite;
 
         if (pieceColor == Color.WHITE) {
@@ -35,7 +40,10 @@ public class BoardConsoleRender {
             result = ANSI_BLACK_PIECE_COLOR + result;
         }
 
-        if(isSquareDark){
+        if(isHighlighted){
+            result = ANSI_HIGHLIGHTED_SQUARE_BACKGROUND + result;
+        }
+        else if(isSquareDark){
             result = ANSI_BLACK_SQUARE_BACKGROUND + result;
         } else {
             result = ANSI_WHITE_SQUARE_BACKGROUND + result;
@@ -44,8 +52,8 @@ public class BoardConsoleRender {
         return result;
      }
 
-     private String getSpriteForEmptySquare(Coordinates coordinates){
-         return colorizeSprite("   ", Color.WHITE, Board.isSqareDark(coordinates));
+     private String getSpriteForEmptySquare(Coordinates coordinates, boolean isHighlight){
+         return colorizeSprite("   ", Color.WHITE, Board.isSqareDark(coordinates), isHighlight);
      }
 
      private String selectUnicodeSpriteForPiece(Piece piece){
@@ -71,8 +79,9 @@ public class BoardConsoleRender {
          return "";
      }
 
-     private String getPieceSprite(Piece piece){
+     private String getPieceSprite(Piece piece, boolean isHighlight){
          return colorizeSprite(
-                 " " + selectUnicodeSpriteForPiece(piece) + " ", piece.color, Board.isSqareDark(piece.coordinates));
+                 " " + selectUnicodeSpriteForPiece(piece) + " ", piece.color, Board.isSqareDark(piece.coordinates),
+                 isHighlight);
      }
 }
--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,17 +1,18 @@
 package chess;
 
-import chess.piece.Piece;
+import chess.piece.BoardFactory;
+ import chess.piece.Piece;
 
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args){
-        Board board = new Board();
-        board.setupDefaultPiecesPositions();
+        Board board = (new BoardFactory()).fromFEN(
+                //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBOKBNR w KQkq - 0 1"
+                "3k4/8/5n2/2N5/3B4/8/8/3K4 w - - 0 1"
+        );
 
-        //int a = 123;
-
-        Game game = new Game(board);
-        game.gameLoop();
+        BoardConsoleRender renderer = new BoardConsoleRender();
+        renderer.render(board);
     }
 }
